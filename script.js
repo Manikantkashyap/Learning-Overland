d962e3dc013c76e5f9d282fa0f9056454f56f3e8
Manikantkashyap <137867117+Manikantkashyap@users.noreply.github.com>
2025-07-16 08:36:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function uploadContent(event) {
+  const file = event.target.files[0];
+  if (file) {
+    alert(`Uploaded: ${file.name}`);
+    // You can implement backend/GitHub upload logic here
+  }
+}
 
